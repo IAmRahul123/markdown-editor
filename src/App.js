94b8fbcbd36dbde4c2c51800ec8b9e717b9ed735
IAmAditya <aditya.gudu@nuvama.com>
2024-07-05 08:53:33 +0530

    added consition for syntax and preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './App.css';
 
 const App = () => {
   const [markdown, setMarkdown] = useState('');
   const [html, setHtml] = useState('');
-  const API_BASE=process.env.REACT_APP_API_URL
+  const [showPreview, setShowPreview] = useState(true);
+  const API_BASE = process.env.REACT_APP_API_URL
 
   const handleMarkdownChange = async (e) => {
     const newMarkdown = e.target.value;
@@ -21,6 +24,9 @@ const App = () => {
     }
   };
 
+  const handleToggle = () => {
+    setShowPreview(prev => !prev)
+  }
   return (
     <div className="App">
       <textarea
@@ -28,10 +34,28 @@ const App = () => {
         onChange={handleMarkdownChange}
         placeholder="Type your markdown here..."
       />
-      <div
-        className="preview"
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></div>
+      <div className="preview">
+        <div className="switch-container">
+          <input
+            type="checkbox"
+            id="switch"
+            checked={showPreview}
+            onChange={handleToggle}
+          />
+          <label htmlFor="switch" className="switch-label">
+            {showPreview ? "Preview" : "Syntax"}
+          </label>
+        </div>
+
+        {showPreview ? <div
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div> :
+          <SyntaxHighlighter language="markdown" style={materialDark}>
+            {html}
+          </SyntaxHighlighter>
+        }
+
+      </div>
     </div>
   );
 }
