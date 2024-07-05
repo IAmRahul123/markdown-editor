e827f4022014dab99983cfdf2c038795e0261079
IAmAditya <aditya.gudu@nuvama.com>
2024-07-05 07:59:50 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const marked = require('marked');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/convert', (req, res) => {
